16ac0647b1425df76402751c54e96866ae03ca3c
Peter Prince <github@pcprince.co.uk>
2019-07-05 13:35:38 +0100

    Add python_tools

---
+++ 2.0/python_tools/arribada_tools/message.py
@@ -0,0 +1,454 @@
+import struct
+import logging
+import inspect
+import sys
+
+logger = logging.getLogger(__name__)
+
+
+class ExceptionMessageInvalidValue(Exception):
+    pass
+
+
+def str_error(error_code):
+    errors = [ 'CMD_NO_ERROR',
+      'CMD_ERROR_FILE_NOT_FOUND',
+      'CMD_ERROR_FILE_ALREADY_EXISTS',
+      'CMD_ERROR_INVALID_CONFIG_TAG',
+      'CMD_ERROR_GPS_COMMS',
+      'CMD_ERROR_TIMEOUT',
+      'CMD_ERROR_CONFIG_PROTECTED',
+      'CMD_ERROR_CONFIG_TAG_NOT_SET',
+      'CMD_ERROR_BRIDGING_DISABLED',
+      'CMD_ERROR_DATA_OVERSIZE',
+      'CMD_ERROR_INVALID_PARAMETER',
+      'CMD_ERROR_INVALID_FW_IMAGE_TYPE',
+      'CMD_ERROR_IMAGE_CRC_MISMATCH',
+      'CMD_ERROR_FILE_INCOMPATIBLE' ]
+    if error_code >= len(errors):
+        return 'ERROR_UNKNOWN'
+    return errors[error_code]
+
+
+def decode(data):
+
+    """Attempt to decode a message from an input data buffer"""
+    hdr = ConfigMessageHeader()
+    if (len(data) < hdr.header_length):
+        return (None, data)
+
+    # If there's no sync byte return
+    if ConfigMessageHeader.sync not in data:
+        return (None, data)
+
+    # Find location of first sync byte and decode from that position
+    pos = data.index(ConfigMessageHeader.sync)
+
+    # Unpack message header at SYNC position
+    hdr.unpack(data[pos:])
+
+    msg = None
+    unused_cls = [ ConfigMessage, ConfigMessageHeader ]
+    for i in inspect.getmembers(sys.modules[__name__], inspect.isclass):
+        cls = i[1]
+        if issubclass(cls, ConfigMessageHeader) and cls not in unused_cls and \
+            hdr.cmd == cls.cmd:
+            msg = cls()
+            break
+
+    if (msg):
+        msg.unpack(data)
+        # Advance buffer past this message
+        data = data[(pos + msg.length):]
+
+    return (msg, data)
+
+
+def convert_to_dict(obj):
+    d = {}
+    ignore = ['error_code', 'cmd', 'sync']
+    for i in obj._args:
+        if i not in ignore:
+            d[i] = getattr(obj, i)
+    return d
+
+
+class _Blob(object):
+    """Blob object is a container for arbitrary message fields which
+    can be packed / unpacked using python struct"""
+    _fmt = ''
+    _args = []
+
+    def __init__(self, fmt, args):
+        self._fmt = fmt
+        self._args = args
+
+    def extend(self, fmt, args):
+        self._fmt += fmt
+        self._args += args
+
+    def pack(self):
+        packer = struct.Struct(self._fmt)
+        args = tuple([getattr(self, k) for k in self._args])
+        return packer.pack(*args)
+
+    def unpack(self, data):
+        unpacker = struct.Struct(self._fmt)
+        unpacked = unpacker.unpack_from(data)
+        i = 0
+        for k in self._args:
+            setattr(self, k, unpacked[i])
+            i += 1
+
+    def __repr__(self):
+        s = self.__class__.__name__ + ' contents:\n'
+        for i in self._args:
+            s += i + ' = ' + str(getattr(self, i, 'undefined')) + '\n'
+        return s
+
+
+class ConfigMessageHeader(_Blob):
+    """Configuration message header"""
+    sync = 0x7E
+
+    def __init__(self, bytes_to_follow=0):
+        _Blob.__init__(self, b'<BB', ['sync', 'cmd'])
+        self.header_length = struct.calcsize(self._fmt)
+        self.length = self.header_length + bytes_to_follow
+
+
+class ConfigMessage(ConfigMessageHeader):
+    """A configuration message which should be subclassed"""
+    def __init__(self, fmt=b'', args=[], **kwargs):
+        ConfigMessageHeader.__init__(self, struct.calcsize(fmt))
+        self.extend(fmt, args)
+        for k in kwargs.keys():
+            setattr(self, k, kwargs[k])
+
+
+class GenericResponse(ConfigMessage):
+    """A generic response message which should be subclassed"""
+    cmd = 0
+    name = 'GENERIC_RESP'
+
+    def __init__(self, **kwargs):
+        ConfigMessage.__init__(self, b'B', ['error_code'], **kwargs)
+
+
+class ConfigMessage_CFG_READ_REQ(ConfigMessage):
+
+    cmd = 1
+    name = 'CFG_READ_REQ'
+
+    def __init__(self, **kwargs):
+        ConfigMessage.__init__(self, b'H', ['cfg_tag'], **kwargs)
+
+
+class ConfigMessage_CFG_READ_RESP(ConfigMessage):
+
+    cmd = 8
+    name = 'CFG_READ_RESP'
+
+    def __init__(self, **kwargs):
+        ConfigMessage.__init__(self, b'BI', ['error_code', 'length'], **kwargs)
+
+
+class ConfigMessage_CFG_WRITE_CNF(ConfigMessage):
+
+    cmd = 9
+    name = 'CFG_WRITE_CNF'
+
+    def __init__(self, **kwargs):
+        ConfigMessage.__init__(self, b'B', ['error_code'], **kwargs)
+
+
+class ConfigMessage_CFG_WRITE_REQ(ConfigMessage):
+
+    cmd = 2
+    name = 'CFG_WRITE_REQ'
+
+    def __init__(self, **kwargs):
+        ConfigMessage.__init__(self, b'I', ['length'], **kwargs)
+
+
+class ConfigMessage_CFG_SAVE_REQ(ConfigMessageHeader):
+
+    cmd = 3
+    name = 'CFG_SAVE_REQ'
+
+
+class ConfigMessage_CFG_RESTORE_REQ(ConfigMessageHeader):
+
+    cmd = 4
+    name = 'CFG_RESTORE_REQ'
+
+
+class ConfigMessage_CFG_ERASE_REQ(ConfigMessage):
+
+    cmd = 5
+    name = 'CFG_ERASE_REQ'
+
+    def __init__(self, **kwargs):
+        ConfigMessage.__init__(self, b'H', ['cfg_tag'], **kwargs)
+
+
+class ConfigMessage_CFG_PROTECT_REQ(ConfigMessageHeader):
+
+    cmd = 6
+    name = 'CFG_PROTECT_REQ'
+
+
+class ConfigMessage_CFG_UNPROTECT_REQ(ConfigMessageHeader):
+
+    cmd = 7
+    name = 'CFG_UNPROTECT_REQ'
+
+
+class ConfigMessage_GPS_WRITE_REQ(ConfigMessage):
+
+    cmd = 10
+    name = 'GPS_WRITE_REQ'
+
+    def __init__(self, **kwargs):
+        ConfigMessage.__init__(self, b'I', ['length'], **kwargs)
+
+
+class ConfigMessage_GPS_READ_REQ(ConfigMessage):
+
+    cmd = 11
+    name = 'GPS_READ_REQ'
+
+    def __init__(self, **kwargs):
+        ConfigMessage.__init__(self, b'I', ['length'], **kwargs)
+
+
+class ConfigMessage_GPS_READ_RESP(ConfigMessage):
+
+    cmd = 12
+    name = 'GPS_READ_RESP'
+
+    def __init__(self, **kwargs):
+        ConfigMessage.__init__(self, b'BI', ['error_code', 'length'], **kwargs)
+
+
+class ConfigMessage_GPS_CONFIG_REQ(ConfigMessage):
+
+    cmd = 13
+    name = 'GPS_CONFIG_REQ'
+
+    def __init__(self, **kwargs):
+        ConfigMessage.__init__(self, b'?', ['enable'], **kwargs)
+
+
+class ConfigMessage_BLE_CONFIG_REQ(ConfigMessage):
+
+    cmd = 14
+    name = 'BLE_CONFIG_REQ'
+
+    def __init__(self, **kwargs):
+        ConfigMessage.__init__(self, b'?', ['enable'], **kwargs)
+
+
+class ConfigMessage_BLE_WRITE_REQ(ConfigMessage):
+
+    cmd = 15
+    name = 'BLE_WRITE_REQ'
+
+    def __init__(self, **kwargs):
+        ConfigMessage.__init__(self, b'BH', ['address', 'length'], **kwargs)
+
+
+class ConfigMessage_BLE_READ_REQ(ConfigMessage):
+
+    cmd = 16
+    name = 'BLE_READ_REQ'
+
+    def __init__(self, **kwargs):
+        ConfigMessage.__init__(self, b'BH', ['address', 'length'], **kwargs)
+
+
+class ConfigMessage_STATUS_REQ(ConfigMessageHeader):
+
+    cmd = 17
+    name = 'STATUS_REQ'
+
+
+class ConfigMessage_STATUS_RESP(ConfigMessage):
+
+    cmd = 18
+    name = 'STATUS_RESP'
+
+    def __init__(self, **kwargs):
+        ConfigMessage.__init__(self, b'BIII', ['error_code', 'fw_version', 'ble_fw_version', 'cfg_version'], **kwargs)
+
+
+class ConfigMessage_FW_SEND_IMAGE_REQ(ConfigMessage):
+
+    cmd = 19
+    name = 'FW_SEND_IMAGE_REQ'
+
+    def __init__(self, **kwargs):
+        ConfigMessage.__init__(self, b'BII', ['image_type', 'length', 'crc',], **kwargs)
+
+    def pack(self):
+        image_type = self.image_type
+        if self.image_type == 'STM32':
+            self.image_type = 1
+        elif self.image_type == 'BLE':
+            self.image_type = 2
+        else:
+            raise ExceptionMessageInvalidValue
+        data = ConfigMessage.pack(self)
+        self.image_type = image_type
+        return data
+
+    def unpack(self, data):
+        ConfigMessage.unpack(self, data)
+        if (self.image_type == 1):
+            self.image_type = 'STM32'
+        elif (self.image_type == 2):
+            self.image_type = 'BLE'
+        else:
+            self.image_type = 'UNKNOWN'
+
+
+class ConfigMessage_FW_SEND_IMAGE_COMPLETE_CNF(GenericResponse):
+
+    cmd = 20
+    name = 'FW_SEND_IMAGE_COMPLETE_CNF'
+
+    def __init__(self, **kwargs):
+        ConfigMessage.__init__(self, b'B', ['error_code'], **kwargs)
+
+
+class ConfigMessage_FW_APPLY_IMAGE_REQ(ConfigMessage):
+
+    cmd = 21
+    name = 'FW_APPLY_IMAGE_REQ'
+
+    def __init__(self, **kwargs):
+        ConfigMessage.__init__(self, b'B', ['image_type'], **kwargs)
+
+    def pack(self):
+        image_type = self.image_type
+        if self.image_type == 'STM32':
+            self.image_type = 1
+        elif self.image_type == 'BLE':
+            self.image_type = 2
+        elif self.image_type == 'SOFTDEVICE':
+            self.image_type = 3
+        else:
+            raise ExceptionMessageInvalidValue
+        data = ConfigMessage.pack(self)
+        self.image_type = image_type
+        return data
+
+    def unpack(self, data):
+        ConfigMessage.unpack(self, data)
+        if (self.image_type == 1):
+            self.image_type = 'STM32'
+        elif (self.image_type == 2):
+            self.image_type = 'BLE'
+        elif (self.image_type == 3):
+            self.image_type = 'SOFTDEVICE'
+        else:
+            self.image_type = 'UNKNOWN'
+
+
+class ConfigMessage_RESET_REQ(ConfigMessage):
+
+    cmd = 22
+    name = 'RESET_REQ'
+
+    def __init__(self, **kwargs):
+        ConfigMessage.__init__(self, b'B', ['reset_type'], **kwargs)
+
+    def pack(self):
+        reset_type = self.reset_type
+        if self.reset_type == 'STM32':
+            self.reset_type = 0
+        elif self.reset_type == 'FLASH':
+            self.reset_type = 1
+        else:
+            raise ExceptionMessageInvalidValue
+        data = ConfigMessage.pack(self)
+        self.reset_type = reset_type
+        return data
+
+    def unpack(self, data):
+        ConfigMessage.unpack(self, data)
+        if (self.reset_type == 0):
+            self.reset_type = 'STM32'
+        elif (self.reset_type == 1):
+            self.reset_type = 'FLASH'
+        else:
+            self.reset_type = 'UNKNOWN'
+
+
+class ConfigMessage_BATTERY_STATUS_REQ(ConfigMessageHeader):
+
+    cmd = 23
+    name = 'BATTERY_STATUS_REQ'
+
+
+class ConfigMessage_BATTERY_STATUS_RESP(ConfigMessage):
+
+    cmd = 24
+    name = 'BATTERY_STATUS_RESP'
+
+    def __init__(self, **kwargs):
+        ConfigMessage.__init__(self, b'B?B', ['error_code', 'charging_ind', 'charging_level'], **kwargs)
+
+
+class ConfigMessage_LOG_CREATE_REQ(ConfigMessage):
+
+    cmd = 25
+    name = 'LOG_CREATE_REQ'
+
+    def __init__(self, **kwargs):
+        ConfigMessage.__init__(self, b'B?', ['mode', 'sync_enable'], **kwargs)
+
+    def pack(self):
+        mode = self.mode
+        if self.mode == 'LINEAR':
+            self.mode = 0
+        elif self.mode == 'CIRCULAR':
+            self.mode = 1
+        else:
+            raise ExceptionMessageInvalidValue
+        data = ConfigMessage.pack(self)
+        self.mode = mode
+        return data
+
+    def unpack(self, data):
+        ConfigMessage.unpack(self, data)
+        if (self.mode == 0):
+            self.mode = 'LINEAR'
+        elif (self.mode == 1):
+            self.mode = 'CIRCULAR'
+        else:
+            self.mode = 'UNKNOWN'
+
+
+class ConfigMessage_LOG_ERASE_REQ(ConfigMessageHeader):
+
+    cmd = 26
+    name = 'LOG_ERASE_REQ'
+
+
+class ConfigMessage_LOG_READ_REQ(ConfigMessage):
+
+    cmd = 27
+    name = 'LOG_READ_REQ'
+
+    def __init__(self, **kwargs):
+        ConfigMessage.__init__(self, b'II', ['start_offset', 'length'], **kwargs)
+
+
+class ConfigMessage_LOG_READ_RESP(ConfigMessage):
+
+    cmd = 28
+    name = 'LOG_READ_RESP'
+
+    def __init__(self, **kwargs):
+        ConfigMessage.__init__(self, b'BI', ['error_code', 'length'], **kwargs)
---
+++ 2.0/python_tools/arribada_tools/pyusb.py
@@ -0,0 +1,173 @@
+import os
+import threading
+import usb
+import logging
+import sys
+
+
+logger = logging.getLogger(__name__)
+
+
+class UsbOverlappedResult(threading._Event):
+    buffer = None
+    status = None
+    cancel = False
+
+
+class UsbOverlappedEndpoint(threading.Thread):
+
+    def __init__(self, ep):
+        self._ep = ep
+        self._is_stopping = False
+        self._queue = []
+        self._event = threading.Event()
+        threading.Thread.__init__(self)
+        self.start()
+
+    def run(self):
+        if (self._ep.bEndpointAddress & 0x80):
+            self._read_handler()
+        else:
+            self._write_handler()
+
+    def _read_handler(self):
+        while (not self._is_stopping):
+            self._event.wait()
+            self._event.clear()
+            while (self._queue):
+                (result, length, timeout, fp) = self._queue.pop(0)
+                if fp is not None:
+                    curr_length = 0
+                    while curr_length < length and not result.cancel:
+                        try:
+                            b = self._ep.read(32768, timeout)
+                            if b:
+                                curr_length = curr_length + len(b)
+                                fp.write(b)
+                        except:
+                            logger.error("Unexpected error: %s", sys.exc_info()[0])
+                            break
+                    result.status = curr_length
+                else:
+                    try:
+                        result.buffer = self._ep.read(length, timeout) or b''
+                        result.status = len(result.buffer)
+                    except:
+                        logger.error("Unexpected error: %s", sys.exc_info()[0])
+                        result.status = -1
+                result.set()
+
+    def _write_handler(self):
+        while (not self._is_stopping):
+            self._event.wait()
+            self._event.clear()
+            while (self._queue):
+                (result, buf, timeout) = self._queue.pop(0)
+                try:
+                    result.status = self._ep.write(buf, timeout)
+                except:
+                    logger.error("Unexpected error: %s", sys.exc_info()[0])
+                    result.status = -1
+                result.set()
+
+    def read(self, length, timeout=None):
+        result = UsbOverlappedResult()
+        result.clear()
+        self._queue += [(result, length, timeout, None)]
+        self._event.set()
+        return result
+
+    def read_to_file(self, length, fp, timeout=None):
+        result = UsbOverlappedResult()
+        result.clear()
+        self._queue += [(result, length, timeout, fp)]
+        self._event.set()
+        return result
+    
+    def write(self, data, timeout=None):
+        result = UsbOverlappedResult()
+        result.clear()
+        self._queue += [(result, data, timeout)]
+        self._event.set()
+        return result
+
+    def stop(self):
+        self._is_stopping = True
+        self._event.set()
+        self.join()
+
+
+class ExceptionUsbDeviceNotFound(Exception):
+    pass
+
+class ExceptionUsbDeviceFailedToClaim(Exception):
+    pass
+
+# Direction is with respect to the host i.e., OUT => write from host to device
+# IN => read from device to host
+EP_MSG_OUT = 0
+EP_MSG_IN = 1
+
+
+class UsbHost():
+
+    VENDOR_ID  = 0x0483     # STMicroelectronics
+    PRODUCT_ID = 0x0100     # Seemingly spare for now
+
+    dev = None
+
+    def __init__(self):
+        self._endpoints = []
+        dev = usb.core.find(find_all=True)
+        devs = [cfg for cfg in dev if cfg.idVendor == UsbHost.VENDOR_ID and cfg.idProduct == UsbHost.PRODUCT_ID]
+        if not devs:
+            raise ExceptionUsbDeviceNotFound
+        # Allow USB_INDEX environment variable to set which device in the
+        # enumeration list we talk to i.e., 0....N-1
+        usb_index = 0 if 'USB_INDEX' not in os.environ else int(os.environ['USB_INDEX'])
+        # Make sure USB_INDEX is in valid range
+        if usb_index >= len(devs):
+            logger.warn('USB_INDEX=%u is out of range - using index 0 instead', usb_index)
+            usb_index = 0
+        self.dev = devs[usb_index]
+        if os.name != 'posix':
+            self.dev.set_configuration()
+        try:
+            usb.util.claim_interface(self.dev, 0)
+            logger.debug("Claimed device")
+        except:
+            logger.error("Failed to claim USB device. Device is already in use or python does not have root permissions")
+            raise ExceptionUsbDeviceFailedToClaim
+        iface = self.dev[0][(0,0)]   # Configuration #0 Interface #0
+        self._endpoints = map(UsbOverlappedEndpoint, iface.endpoints())
+        # Read all avaliable data to flush the targets TX buffer
+        start_log_level = logger.getEffectiveLevel()
+        logger.setLevel(logging.CRITICAL)
+        while True:
+            resp = self._endpoints[EP_MSG_IN].read(512, 50)
+            resp.wait()
+            if resp.status == -1:
+                break
+        logger.setLevel(start_log_level)
+
+    def read(self, idx, length, timeout=None):
+        logger.debug('USB read ep %u', idx)
+        return self._endpoints[idx].read(length, timeout)
+
+    def read_to_file(self, idx, length, fp, timeout=None):
+        logger.debug('USB read to file ep %u len %u fp %s', idx, length, fp)
+        return self._endpoints[idx].read_to_file(length, fp, timeout)
+
+    def write(self, idx, data, timeout=None):
+        logger.debug('USB write ep %u', idx)
+        return self._endpoints[idx].write(data, timeout)
+
+    def cleanup(self):
+        if self._endpoints:
+            [ep.stop() for ep in self._endpoints]
+        self._endpoints = None
+        if self.dev is not None:
+            usb.util.dispose_resources(self.dev) # Release interface
+
+    def __del__(self):
+        self.cleanup()
